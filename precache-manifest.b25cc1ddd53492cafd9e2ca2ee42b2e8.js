000b392fd092bcbdc45c592f0948f81cd9f38f2b
bbydino <daniel.ngo16@gmail.com>
2020-12-14 18:08:19 -0600

    Updates

---
+++ precache-manifest.b25cc1ddd53492cafd9e2ca2ee42b2e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13423d860f75607e0e6a8718a4819231",
+    "url": "/smexy-stock-market/index.html"
+  },
+  {
+    "revision": "b7ed61510375b41f30d5",
+    "url": "/smexy-stock-market/static/css/main.8aff59d5.chunk.css"
+  },
+  {
+    "revision": "dd1f51062621223c5f0b",
+    "url": "/smexy-stock-market/static/js/2.1f3c474d.chunk.js"
+  },
+  {
+    "revision": "28bcb252a64156eb2fda27866b95e924",
+    "url": "/smexy-stock-market/static/js/2.1f3c474d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ed61510375b41f30d5",
+    "url": "/smexy-stock-market/static/js/main.d2d0c052.chunk.js"
+  },
+  {
+    "revision": "259ac2e0513c65abe973",
+    "url": "/smexy-stock-market/static/js/runtime-main.5f0b98ad.js"
+  },
+  {
+    "revision": "b451faa2724e32797ed6db1845e5814a",
+    "url": "/smexy-stock-market/static/media/bg.b451faa2.jpg"
+  }
+]);
